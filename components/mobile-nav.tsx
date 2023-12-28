0f68ed091c2675d530a1aab544bf6f97a47ec163
floating-dynamo <shridharmaskeri@gmail.com>
2023-12-28 22:00:21 +0530

    Used new Drawer component for mobile menu

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,19 +1,17 @@
-import { Menu } from "lucide-react";
-import React from "react";
+import { Menu } from 'lucide-react';
+import React from 'react';
 import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { Button } from "@/components/ui/button";
-import { NavItem } from "@/types/nav";
-import { NavbarItems } from "./main-nav";
-import { Icons } from "./icons";
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { Button } from '@/components/ui/button';
+import { NavItem } from '@/types/nav';
+import { NavbarItems } from './main-nav';
+import { Icons } from './icons';
 
 interface MobileNavProps {
   items?: NavItem[];
@@ -23,29 +21,23 @@ interface MobileNavProps {
 
 const MobileNav = ({ items, divClasses, linkClasses }: MobileNavProps) => {
   return (
-    <Sheet>
-      <SheetTrigger asChild className="flex md:hidden">
-        <Button variant="outline">
+    <Drawer>
+      <DrawerTrigger asChild className='flex md:hidden'>
+        <Button variant='outline'>
           <Menu />
         </Button>
-      </SheetTrigger>
-      <SheetContent side={"left"}>
-        <SheetHeader className="flex flex-col items-center">
-          <Icons.logo className="h-12 w-12 mb-5" />
-          <SheetTitle>Sridhar Maskeri</SheetTitle>
-          <SheetDescription>SWE | Web Developer | Graphic Designer</SheetDescription>
-        </SheetHeader>
-        <div className="flex gap-4 py-4 flex-col h-2/3 mt-10">
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className='flex flex-col items-center'>
+          <Icons.logo className='h-12 w-12 mb-5' />
+          <DrawerTitle>Sridhar Maskeri</DrawerTitle>
+          <DrawerDescription>SWE | Web Developer | Graphic Designer</DrawerDescription>
+        </DrawerHeader>
+        <div className='flex gap-4 py-4 flex-col h-2/3 mt-10'>
           <NavbarItems items={items} divClasses={divClasses} linkClasses={linkClasses} />
         </div>
-        <SheetFooter>
-          {/* <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose> */}
-          {/* <SheetDescription>Sridhar Maskeri ©️ 2023-2024</SheetDescription> */}
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
